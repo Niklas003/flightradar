d9ff42369a5199ed8bb508c48ac10206e8ad92e2
Niklas Labitzke <niklas.labitzke@deutschebahn.com>
2025-08-08 18:37:24 +0200

    vercel config

--- api/index.ts
+++ api/index.ts
@@ -10,6 +10,7 @@ import {
 const app = express();
 const port = 4000;
 app.listen(port, () => console.log(`The server is listening on port ${port}`));
+module.exports = app; // Export the app for testing
 app.use("/logos", express.static(path.join(process.cwd(), "public", "logos")));
 app.use(
   "/banners",
